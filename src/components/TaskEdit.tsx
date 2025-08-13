fee704c1e5c312da3e26406c780640eff594ea44
Fred <gasolin@gmail.com>
2025-08-13 22:14:30 +0800

    remove unused import

--- src/components/TaskEdit.tsx
+++ src/components/TaskEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Box, Text, useApp, useInput } from 'ink'
 import { TodoManager } from '../lib/TodoManager.js'
 import { Task } from '../types/Task.js'
